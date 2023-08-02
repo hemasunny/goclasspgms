479078f5b82c7b9e8424657c3b34bfa71e2e9b15
hema*g1thub <hemasunny@gmail.com>
2023-08-02 16:57:08 +0530

    first

---
+++ pgm1/package1/a1.go
@@ -0,0 +1,13 @@
+package package1
+
+import "log"
+
+func Subtract() {
+
+	log.Println("Hello from subtract")
+}
+
+func Add() {
+
+	log.Println("Hello from add fn")
+}
---
+++ pgm1/start.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hemasunny/pgm1/package1"
+)
+
+func main() {
+
+	log.Println("Hello World")
+
+	package1.Subtract()
+	package1.Add()
+}
